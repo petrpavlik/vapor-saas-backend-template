749a88c5b2a7d3bbec950170b811e731a3ccf94f
Petr Pavlik <petrpavlik@me.com>
2024-01-21 15:01:59 +0100

    abstract firebase auth

---
+++ Sources/App/Auth/FirebaseAuth.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by Petr Pavlik on 21.01.2024.
+//
+
+import Vapor
+import FirebaseJWTMiddleware
+
+protocol JWTUser {
+    var userID: String { get }
+    var email: String? { get }
+}
+
+extension FirebaseJWTPayload : JWTUser {
+    
+}
+
+extension Request {
+    var jwtUser: some JWTUser {
+        get async throws {
+            // This is where you can swap Firebase Auth for another JWT-based provider (Amazon Congnito, Clerk, ...)
+            try await self.firebaseJwt.asyncVerify()
+        }
+    }
+}
